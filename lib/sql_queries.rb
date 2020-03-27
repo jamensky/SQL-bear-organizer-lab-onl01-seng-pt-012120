67e44de5ebdea298fbbb71aa91aba39c085d6a6d
Jamal Rjoul <jamensky1991@gmail.com>
2020-03-27 10:24:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE gender = F;"
+  binding.pry
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
